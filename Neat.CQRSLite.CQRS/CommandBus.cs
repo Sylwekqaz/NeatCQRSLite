1a75d7636b0bc7c3e200599c7a9c645be177a02f
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-03-10 19:49:02 +0100

    Dodanie szyny komend

---
+++ Neat.CQRSLite.CQRS/CommandBus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Neat.CQRSLite.Contract.Commands;
+
+namespace Neat.CQRSLite.CQRS
+{
+    public class CommandBus : ICommandBus
+    {
+        private readonly Func<Type, ICommandValidator<ICommand>> _validatorResolver;
+        private readonly Func<Type, ICommandHandler<ICommand>> _commandHandlerResolver;
+
+        public CommandBus(Func<Type, ICommandValidator<ICommand>> validatorResolver,
+            Func<Type, ICommandHandler<ICommand>> commandHandlerResolver)
+        {
+            _validatorResolver = validatorResolver;
+            _commandHandlerResolver = commandHandlerResolver;
+        }
+
+        public CommandResult Execute<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            var commandValidator = _validatorResolver(typeof(TCommand));
+            if (commandValidator != null)
+            {
+                var validationErrors = commandValidator.Validate(command);
+                if (validationErrors.Any())
+                {
+                    return CommandResult.NotValid(validationErrors);
+                }
+            }
+            _commandHandlerResolver(typeof(TCommand)).Execute(command);
+            return CommandResult.Success();
+        }
+    }
+}
