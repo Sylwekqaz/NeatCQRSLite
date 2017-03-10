7818928a5b2abd2930a71b4111006e49bb2a9fa6
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-03-10 19:45:04 +0100

    Dodanie interfejsu dla aggregate root

---
+++ Neat.CQRSLite.Contract/Domain/IAggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Neat.CQRSLite.Contract.Domain
+{
+    public interface IAggregateRoot<out TKey>
+    {
+        TKey Id { get; }
+    }
+
+    public interface IAggregateRoot : IAggregateRoot<Guid>
+    {
+    }
+}
