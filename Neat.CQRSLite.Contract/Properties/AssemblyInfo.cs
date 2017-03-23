fd32eaec29c46de8d6df339ef67de703ed41c069
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-03-23 13:45:01 +0100

    Poprawienia AssemblyInfo dla projektu contract

--- Neat.CQRSLite.Contract/Properties/AssemblyInfo.cs
+++ Neat.CQRSLite.Contract/Properties/AssemblyInfo.cs
@@ -5,12 +5,16 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neat.CQRSLite.Contract")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("CQRSLite - Contract")]
+[assembly: AssemblyDescription("NeatCQRSLite is simple CQRS (command query responsibility segregation) framework. This peoject contain implementaton of CQRS basic bus definied in contract project")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+ [assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Sylwekqaz, NeatCode")]
 [assembly: AssemblyProduct("Neat.CQRSLite.Contract")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © NeatCode 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +36,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//Assembly versions are patched by appveyor build
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-local-build")]
+
