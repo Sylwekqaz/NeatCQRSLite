693ad825615610d1d372fee5f0268bacc0e89b5d
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-04-19 14:44:38 +0200

    Przepisanie IAssistant  na klasę

---
+++ src/Neat.CQRSLite.Contract/Helpers/Assistant.cs
@@ -0,0 +1,40 @@
+﻿using Neat.CQRSLite.Contract.Commands;
+using Neat.CQRSLite.Contract.Events;
+using Neat.CQRSLite.Contract.Queries;
+
+namespace Neat.CQRSLite.Contract.Helpers
+{
+    public class Assistant
+    {
+        private readonly IEventBus _eventBus;
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryBus _queryBus;
+
+        public Assistant(IEventBus eventBus, ICommandBus commandBus, IQueryBus queryBus)
+        {
+            _eventBus = eventBus;
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+        }
+
+        public CommandResult Do<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            return _commandBus.Execute(command);
+        }
+
+        public void Tell<TEvent>(TEvent @event) where TEvent : IEvent
+        {
+            _eventBus.Send(@event);
+        }
+
+        public TResult Give<TResult>(IQuery<TResult> query)
+        {
+            return _queryBus.Perform(query);
+        }
+
+        public bool Check<TQuery>(TQuery query) where TQuery : IQuery<bool>
+        {
+            return _queryBus.Perform(query);
+        }
+    }
+}
