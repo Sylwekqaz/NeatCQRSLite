d1eaeadaaaaf39a87a8e5aff84fc9630cd21161a
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-04-24 14:06:37 +0200

    stasz

--- Example/ExampleAspMVCApp/ExampleAspMVCApp/App_Start/NinjectWebCommon.cs
+++ Example/ExampleAspMVCApp/ExampleAspMVCApp/App_Start/NinjectWebCommon.cs
@@ -1,38 +1,46 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ExampleAspMVCApp.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(ExampleAspMVCApp.App_Start.NinjectWebCommon), "Stop")]
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExampleAspMVCApp;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Neat.CQRSLite.Contract.Helpers;
+using Neat.CQRSLite.Contract.Commands;
+using Neat.CQRSLite.Contract.Events;
+using Neat.CQRSLite.Contract.Queries;
+using Neat.CQRSLite.CQRS;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using Ninject.Web.Common;
 
-namespace ExampleAspMVCApp.App_Start
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly:
+    WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace ExampleAspMVCApp
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
-        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            bootstrapper.Initialize(CreateKernel);
+            Bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
         public static void Stop()
         {
-            bootstrapper.ShutDown();
+            Bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -61,6 +69,41 @@ namespace ExampleAspMVCApp.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+            //bind all buss
+            kernel.Bind<IQueryBus>().To<QueryBus>();
+            kernel.Bind<IEventBus>().To<EventBus>();
+            kernel.Bind<ICommandBus>().To<CommandBus>();
+
+            //bind multibuss
+            kernel.Bind<Neat.CQRSLite.Contract.IAssistant Assistant>().ToSelf();
+
+
+            // bind resolving functions 
+            kernel.Bind<Func<Type, IQueryPerformer>>()
+                .ToMethod(c => (T => (IQueryPerformer)c.Kernel.Get(T)));
+            kernel.Bind<Func<Type, ICommandHandler>>()
+                .ToMethod(c => (T => (ICommandHandler)c.Kernel.Get(T)));
+            kernel.Bind<Func<Type, ICommandValidator>>()
+                .ToMethod(c => (T => (ICommandValidator)c.Kernel.Get(T)));
+            kernel.Bind<Func<Type, IEnumerable<IEventHandler>>>()
+                .ToMethod(c => (T => c.Kernel.GetAll(T).Cast<IEventHandler>()));
+
+
+            //bind all handlers
+            kernel.Bind(x =>
+            {
+                x
+                    .FromThisAssembly().SelectAllClasses()
+                    .InheritedFrom(typeof(ICommandHandler<>))
+                    .Join.FromThisAssembly().SelectAllClasses()
+                    .InheritedFrom(typeof(ICommandValidator<>))
+                    .Join.FromThisAssembly().SelectAllClasses()
+                    .InheritedFrom(typeof(IEventHandler<>))
+                    .Join.FromThisAssembly().SelectAllClasses()
+                    .InheritedFrom(typeof(IQueryPerformer<,>))
+                    .BindSingleInterface()
+                    .Configure(b => b.InTransientScope());
+            });
+        }
     }
 }
